709aa00cefa063518c6d3b867c9436d888096fa8
Carlos Lopez <cargalladasch@gmail.com>
2017-12-20 20:59:05 +0100

    Creada primeira estructura de carpetas

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 // importar
 var express = require('express');
+var iniciador = require('./servidor/src/iniciador.js');
  
 // instanciar
 var app = express();
  
 // ruteo
 app.get('/', function(req, res){
-  res.sendfile(__dirname + '/index.html');
+  res.sendfile(__dirname + '/cliente/index.html');
 });
 
-// escuchar
-app.listen(80);
+iniciador.init(app);
  
 console.log("Servidor Express escuchando en modo %s", app.settings.env);
---
+++ servidor/src/iniciador.js
@@ -0,0 +1,7 @@
+function init(app){
+    app.listen(80);
+}
+
+module.exports = {
+    init: init
+}
